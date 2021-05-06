d4401a2d7d84b36015102f361b6c070326a8d9ee
Alejandro Rueda <alejandrorueda@Alejandros-MacBook-Pro.local>
2021-05-06 18:52:52 -0500

    Voyager 3

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
-});
